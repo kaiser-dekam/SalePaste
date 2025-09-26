53736ef503a9424cc244e05d32ae7e6c61841ade
Kaiser De Kam <kaiserddekam@gmail.com>
2025-09-26 09:41:59 -0400

    Initial Commit

---
+++ SalePaste/SalePasteApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalePasteApp.swift
+//  SalePaste
+//
+//  Created by Kaiser DeKam on 9/26/25.
+//
+
+import SwiftUI
+
+@main
+struct SalePasteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SalePasteTests/SalePasteTests.swift
@@ -0,0 +1,17 @@
+//
+//  SalePasteTests.swift
+//  SalePasteTests
+//
+//  Created by Kaiser DeKam on 9/26/25.
+//
+
+import Testing
+@testable import SalePaste
+
+struct SalePasteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
